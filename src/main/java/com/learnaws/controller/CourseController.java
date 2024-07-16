c7140a87340b316feddc7e3f70a82e1e10a8216a
Mishti Sachin <sgs.engineer@gmail.com>
2024-07-16 06:03:06 +0530

    new endpoint added

--- src/main/java/com/learnaws/controller/CourseController.java
+++ src/main/java/com/learnaws/controller/CourseController.java
@@ -57,11 +57,10 @@ public class CourseController {
         }
     }
 
-    /*@GetMapping("/health")
+    @GetMapping("/health")
     public String healthCheck() {
         return "UP";
-    }*/
-
+    }
 
 
     @GetMapping("/welcome")
